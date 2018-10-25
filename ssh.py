5757605501afa56f37a2b73b51bd3a17051ee8dc
rohman <44226301+rohmanpendo@users.noreply.github.com>
2018-10-25 10:44:56 +0700

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,40 +1,31 @@
 import paramiko
 from getpass import getpass
 
-hostname = "192.168.56.101"
-port = 21112
-#username = input ("Usename : ")
-#password = getpass()
-command = input("command : ")
-
-username = "eos" 
-password = "namakulah"
-#command = "fdisk -l"
-#command = ["hostnamectl,ls /etc/ssh/"]
-
+hostname = "hostname"
+port = 22
+username = "username"
+password = "password"
+command = "ls /home"
 
 try :
 	client=paramiko.SSHClient()
 	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	client.connect(hostname = hostname, port = port, username= username, password = password)
 	print ("\nSSH connection to %s established.\n\n" %hostname)
-	output = ""
+	hasil = ""
 	stdin, stdout, stderr = client.exec_command(command)
-	#stdin.write(password)
-	#stdin.flush()
 except paramiko.SSHException as sshException:
 	print ("\n%s\n\n" %sshException)
 	result_flag = False
 
 stdout=stdout.readlines()
 
-#print ("Command : "+command+"\n")
 for line in stdout:
-    output = output + line
-if output != "":
-    print (output)
+    hasil = hasil + line
+if hasil != "":
+    print (hasil)
 else:
-    print ("There was no output for this command")
+    print ("There was no result for this command")
 
 client.close()
 print ("\n\nLogged out\nConnection to %s closed.\n" %hostname)
